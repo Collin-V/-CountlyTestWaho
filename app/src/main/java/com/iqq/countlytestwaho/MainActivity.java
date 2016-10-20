94ae51442975d779e25d371bbefa2ff79e80b01f
Collin-V <sojen3@126.com>
2016-10-20 09:47:58 +0800

    My first commit on git!

---
+++ app/src/main/java/com/iqq/countlytestwaho/MainActivity.java
@@ -0,0 +1,23 @@
+package com.iqq.countlytestwaho;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button bn = (Button) findViewById(R.id.ok);
+        bn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final TextView show = (TextView) findViewById(R.id.show);
+                show.setText("Hello Android~" + new java.util.Date());
+            }
+        });
+    }
+}
